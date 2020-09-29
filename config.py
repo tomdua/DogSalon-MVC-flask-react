c16c13ecfd80b568433ef6feeb9c7c47eb0b502f
tomdu <46071208+tomdua@users.noreply.github.com>
2020-09-29 22:13:54 +0300

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-config = {}
-config['SECRET_KEY'] = 'qwe123'
-config['MONGO_DBNAME'] = 'MyDog'
-config["MONGO_URI"] = "mongodb+srv://tom:qwe123@cluster0.1sze0.mongodb.net/MyDog?retryWrites=true&w=majority"
